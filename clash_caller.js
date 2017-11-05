f74d135cbfe1c022f2da93077f2141c869c8890b
demogrevo <32991859+demogrevo@users.noreply.github.com>
2017-11-05 12:13:52 -0500

    Update clash_caller.js

--- clash_caller.js
+++ clash_caller.js
@@ -14,8 +14,8 @@ db_url = process.env.CLEARDB_DATABASE_URL;
 var conn = mysql.createConnection(db_url);
 
 
-var my_clan_name = "Your clan name here"; // Your clan name
-var war_call_timer = 6; // Timer for calls. How long until a call expires.
+var my_clan_name = "Reddit Phoenix"; // Your clan name
+var war_call_timer = -4; // Timer for calls. How long until a call expires.
 var user_name = "";
 var user_id = "";
 var caller_code = "";
